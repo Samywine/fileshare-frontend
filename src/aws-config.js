98ace90d6e09f0f0b038de7db3eb55bdf8004c23
Samuel Ebosele <samywine2@gmail.com>
2026-04-07 03:13:55 -0400

    rollback to stable amplify v5

--- src/aws-config.js
+++ src/aws-config.js
@@ -1,30 +1,12 @@
 const awsConfig = {
-  Auth: {
-    Cognito: {
-      userPoolId: "us-east-1_V1J5NEoSe",
-      userPoolClientId: "7qmdmue2pkr8c73cgmsedlmhil",
-      loginWith: {
-        email: true
-      }
-    }
-  },
-  API: {
-    GraphQL: {
-      endpoint: "https://ga4mnxazezc6dcodscspliuvtq.appsync-api.us-east-1.amazonaws.com/graphql",
-      region: "us-east-1",
-      defaultAuthMode: "userPool"
-    }
-  },
-
-  // ✅ This is the fixed corrected name Amplify now requires
-  options: {
-    graphql: {
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      }
-    }
-  }
+  aws_project_region: "us-east-1",
+  aws_cognito_region: "us-east-1",
+  aws_user_pools_id: "us-east-1_V1J5NEoSe",
+  aws_user_pools_web_client_id: "7qmdmue2pkr8c73cgmsedlmhil",
+  aws_appsync_graphqlEndpoint:
+   "https://ga4mnxazezc6dcodscspliuvtq.appsync-api.us-east-1.amazonaws.com/graphql",
+  aws_appsync_region: "us-east-1",
+  aws_appsync_authenticationType: "AMAZON_COGNITO_USER_POOLS"
 };
 
 export default awsConfig;
